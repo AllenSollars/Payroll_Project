748fca195ae3784aaa52519eb8ebb6dde7dd5a04
Allen <allensollars03@gmail.com>
2025-09-16 16:13:10 -0400

    completed up to last 2 parts

--- Payroll.py
+++ Payroll.py
@@ -1,24 +1,42 @@
-name=input("What is your name?")
+payrate = 13.75
+overtimeMultiplier = 1.5
+stateTaxRate = 0.056
+federalTaxRate = 0.079
+maxHours = 40
+
+name = input("What is your name?")
 print("Hello, "+ name  )
 print("Next")
 
 #prompt the user for their ID number.
 
-user_id=input("Please enter your ID number: ")
-print(f"You entered ID: {user_id}")
+userId = input("Please enter your ID number: ")
+print(f"You entered ID: {userId}")
 print("Thank you, "+ name)
 print("next")
 
 #prompt the user for The amount of hours worked for the week.
 
-hours_worked=input("Enter the amount of hours worked this week:")
-print(f"You entered: {hours_worked}")
-print("Thank you, "+ name)
-print("Thank you we will register the information shortly")
+hoursWorked = float(input("Enter the amount of hours worked this week (max 40): "))
+print(f"You entered: {hoursWorked}")
+
+overtimeHours = float(input("Enter overtime if any otherwise enter 0: "))
+print(f"You entered: {overtimeHours}")
+
+print("Thank you, " + name)
+print("Thank you, we will register the information shortly")
 
 
-if (float(hours_worked)) <= 40: 
-    grossPay = (float(hours_worked)) * 13.75
-    print(grossPay)
+if hoursWorked <= maxHours and overtimeHours == 0:
+    grossPay = hoursWorked * payrate
+    overtimePay = 0
 else:
-    print("error, too many hours worked")
+    grossPay = hoursWorked * payrate
+    overtimePay = overtimeHours * payrate * overtimeMultiplier
+
+stateTax = grossPay * stateTaxRate
+federalTax = grossPay *federalTaxRate
+totalTax = stateTax + federalTax
+
+netPay = grossPay - totalTax
+
